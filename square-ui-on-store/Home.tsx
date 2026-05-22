f550a2eb56cb499cac7a3ed1760af0eb38d8dfad
navingurung <navin.gurung@tai-matsu.jp>
2026-05-22 15:10:25 +0900

    Update Home.tsx

--- square-ui-on-store/Home.tsx
+++ square-ui-on-store/Home.tsx
@@ -1,3 +1,8 @@
 ```tsx
   const isSquare = loginShop.use_square || true; // Check if Square integration is enabled
+
+
+  // 401
+            isSquare,// Pass the Square integration flag to the Order component
+
 ```
